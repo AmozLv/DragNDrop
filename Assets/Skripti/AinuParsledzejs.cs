eabd14b32b0c042bcd103937ae830ab16700972a
unknown <alekss.mozajevs@sk.lvt.lv>
2023-06-07 10:16:26 +0300

    Projekts pabeigts

--- Assets/Skripti/AinuParsledzejs.cs
+++ Assets/Skripti/AinuParsledzejs.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class AinuParsledzejs : MonoBehaviour {
+
+
+		void Start()
+		{
+			// Switch to 1280 x 600 
+		Screen.SetResolution(1280, 600, false);
+		}
 	public void UzSakumu(){
 		SceneManager.LoadScene ("Sakums", LoadSceneMode.Single);
 	}
